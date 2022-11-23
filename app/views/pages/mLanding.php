f23d0f46480ca667e5c83296935481343e12577c
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-11-23 15:53:44 -0600

    Inicio del modulo de mercadotecnia

---
+++ app/views/pages/mLanding.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php include('../includes/headDashboard.php') ?>
+
+<!-- body start -->
+
+<body class="loading" data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": true}, "topbar": {"color": "light"}, "showRightSidebarOnPageLoad": true}'>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+        <!-- Topbar Start -->
+        <?php include('../includes/topbar.php') ?>
+
+        <!-- ========== Left Sidebar Start ========== -->
+        <?php include('../includes/leftSidebar.php') ?>
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+        <div class="content-page">
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="card">
+                            <div class="card-body">
+                                <h2>Estadisticas</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+    <!-- Rightbar Start -->
+    <?php include('../includes/rightbar.php') ?>
+    <!--Scripts  -->
+    <?php include('../includes/scriptsDashboard.php'); ?>
+</body>
+
+</html>
