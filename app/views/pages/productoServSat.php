2208d9d3fc160e4c5fe3221af2d4633ca6f2329e
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-11-25 18:35:28 -0600

    Actualizacion

---
+++ app/views/pages/productoServSat.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('../includes/headDashboard.php') ?>
+
+<!-- body start -->
+
+<body class="loading" data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": true}, "topbar": {"color": "light"}, "showRightSidebarOnPageLoad": true}'>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+        <!-- Topbar Start -->
+        <?php include('../includes/topbar.php') ?>
+
+        <!-- ========== Left Sidebar Start ========== -->
+        <?php include('../includes/leftSidebar.php') ?>
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+        <div class="content-page">
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <h1>Productos Y Servicios del SAT</h1>
+                        <a href="./datosBancarios-edit.php" class="btn btn-success waves-effect waves-light col-4  col-md-2 col-sm-4 m-2">
+                            Agregar Producto/Servicio SAT
+                        </a>
+                        <!-- Filtros search init-->
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table mb-0">
+                                            <thead class="table-dark">
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Banco</th>
+                                                    <th>Cuenta</th>
+                                                    <th>Beneficiario</th>
+                                                    <th>Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th>1 </th>
+                                                    <td>Banco</td>
+                                                    <td>747587634652858 </td>
+                                                    <td>Juan Peréz</td>
+                                                    <td>
+                                                        <a href="./datosBancarios-edit.php" class="btn btn-info waves-effect waves-light">
+                                                            <i class="fa fa-edit"></i>
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Filtros search fin-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- END wrapper -->
+    <!-- Rightbar Start -->
+    <?php include('../includes/rightbar.php') ?>
+    <!--Scripts  -->
+    <?php include('../includes/scriptsDashboard.php'); ?>
+</body>
+
+</html>
