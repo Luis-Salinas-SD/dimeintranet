ab0e0580d57fa83a9694e1176a42567136fc4bea
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-12-03 13:23:08 -0600

    Actualizacion

--- app/views/includes/headeLogin.php
+++ app/views/includes/headeLogin.php
@@ -7,14 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="img/favicon.png">
-
     <!-- App css -->
     <link href="css/config/default/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
     <link href="css/config/default/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
-
     <link href="css/config/default/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled="disabled" />
     <link href="css/config/default/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" disabled="disabled" />
-
     <!-- icons -->
     <link href="css/icons.min.css" rel="stylesheet" type="text/css" />
     <!-- SweetAlert -->
