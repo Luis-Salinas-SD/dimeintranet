fe3629fddd0be06c5428d27569483dcbf752989d
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-12-05 10:51:49 -0600

    se agrego validación para produccion

--- app/controller/validate.php
+++ app/controller/validate.php
@@ -9,7 +9,27 @@ if (isset($_POST['email']) and isset($_POST['password'])) {
     } elseif (($email === 'roberto@dime.pro') and ($password === 'impresora2')) {
         header('Location:  https://dime-intranet/app/views/pages/elegir.php');
         exit;
-    }else{
+    } else {
         echo 'Datos incorrectos';
     }
 }
+
+//! Validacion para el servidor
+
+/*
+if (isset($_POST['email']) and isset($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (($email === 'luis@dime.pro') and ($password === 'impresora1')
+    ) {
+        header('Location:  https://suwebexpress.com.mx/dimetest/app/views/pages/dashboard.php');
+        exit;
+    } elseif (($email === 'roberto@dime.pro') and ($password === 'impresora2')) {
+        header('Location:  https://suwebexpress.com.mx/dimetest/app/views/pages/elegir.php');
+        exit;
+    } else {
+        echo 'Datos incorrectos';
+    }
+}
+*/
