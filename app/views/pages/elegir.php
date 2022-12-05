42054bea08ab7af6facdeb64b32a2d76ee6bd571
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-12-05 16:42:53 -0600

    Eleccion de empresa corregido

--- app/views/pages/elegir.php
+++ app/views/pages/elegir.php
@@ -4,58 +4,51 @@
 
 <!-- body start -->
 
-<body class="loading"
-    data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": true}, "topbar": {"color": "light"}, "showRightSidebarOnPageLoad": true}'>
+<body class="loading" data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": true}, "topbar": {"color": "light"}, "showRightSidebarOnPageLoad": true}'>
 
     <!-- Begin page -->
     <div id="wrapper">
-        <!-- Topbar Start -->
-        <?php include('../includes/topbar.php') ?>
-
-        <!-- ========== Left Sidebar Start ========== -->
-        <?php include('../includes/leftSidebar.php') ?>
 
         <!-- ============================================================== -->
         <!-- Start Page Content here -->
         <!-- ============================================================== -->
-        <div class="content-page">
-            <div class="content">
+        <div class="card">
+            <div class="card-body">
                 <div class="container-fluid">
-                    <div class="row">
+                    <div class="row text-center">
                         <h1> Empresas </h1>
-                        <!-- Empresa 1 -->
-                        <div class="col-md-6 col-xl-3">
-                            <!-- Card Dime -->
-                            <div class="card">
-                                <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg"
-                                    alt="imagen Dime">
-                                <div class="card-body text-center">
-                                    <h4 class="card-title">Dime Estudio de diseño</h4>
-                                    <a href="../pages/dashboard.php" class="btn btn-primary">Acceder</a>
+                        <div class="d-flex justify-content-around flex-wrap">
+                            <!-- Empresa 1 -->
+                            <div class="col-md-6 col-xl-3 p-3">
+                                <!-- Card Dime -->
+                                <div class="card">
+                                    <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg" alt="imagen Dime">
+                                    <div class="card-body text-center">
+                                        <h4 class="card-title">Dime Estudio de diseño</h4>
+                                        <a href="../pages/dashboard.php" class="btn btn-primary">Acceder</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- Empresa 2 -->
-                        <div class="col-md-6 col-xl-3">
-                            <!-- Card Imprenta -->
-                            <div class="card">
-                                <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg"
-                                    alt="imagen Tu imprenta">
-                                <div class="card-body text-center">
-                                    <h4 class="card-title">Tu Imprenta</h4>
-                                    <a href="#" class="btn btn-success">Acceder</a>
+                            <!-- Empresa 2 -->
+                            <div class="col-md-6 col-xl-3 p-3">
+                                <!-- Card Imprenta -->
+                                <div class="card">
+                                    <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg" alt="imagen Tu imprenta">
+                                    <div class="card-body text-center">
+                                        <h4 class="card-title">Tu Imprenta</h4>
+                                        <a href="#" class="btn btn-success">Acceder</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- Empresa 3 -->
-                        <div class="col-md-6 col-xl-3">
-                            <!-- Card Imprenta -->
-                            <div class="card">
-                                <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg"
-                                    alt="imagen Tu imprenta">
-                                <div class="card-body text-center">
-                                    <h4 class="card-title">Estética</h4>
-                                    <a href="#" class="btn btn-warning">Acceder</a>
+                            <!-- Empresa 3 -->
+                            <div class="col-md-6 col-xl-3 p-3">
+                                <!-- Card Imprenta -->
+                                <div class="card">
+                                    <img class="card-img-top img-fluid" src="../../../public/img/gallery/1.jpg" alt="imagen Tu imprenta">
+                                    <div class="card-body text-center">
+                                        <h4 class="card-title">Estética</h4>
+                                        <a href="#" class="btn btn-warning">Acceder</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
