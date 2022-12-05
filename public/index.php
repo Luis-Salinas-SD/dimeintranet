264efd717396a67753e659ef12b8797c31986952
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-12-05 16:16:18 -0600

    Correccion del login - Responsivo

--- public/index.php
+++ public/index.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<link href="./css/config/default/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-<link href="./css/config/default/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
-<link href="./css/config/default/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled="disabled" />
-<link href="./css/config/default/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" disabled="disabled" />
+<head>
+    <meta charset="utf-8" />
+    <title>Register & Signup | Adminto - Responsive Admin Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+    <!-- App css -->
+    <link href="./css/config/default/bootstrap.min.css" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="./css/config/default/app.min.css" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+    <link href="./css/config/default/bootstrap-dark.min.css" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" disabled="disabled" />
+    <link href="./css/config/default/app-dark.min.css" rel="stylesheet" type="text/css" id="app-dark-stylesheet" disabled="disabled" />
+
+    <!-- icons -->
+    <link href="./css/icons.min.css" rel="stylesheet" type="text/css" />
+
+</head>
 
 <body class="loading authentication-bg authentication-bg-pattern">
-    <div class="account-pages my-5">
+
+    <div class="account-pages mt-5 mb-5">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-4">
@@ -53,30 +70,20 @@
                     </div>
                     <!-- end card -->
 
-                    <div class="row mt-3">
-                        <div class="col-12 text-center">
-                            <p>
-                                <a href="pages-recoverpw.html" class="text-muted ms-1"><i class="fa fa-lock me-1"></i>¿Olvidó su contraseña?</a>
-                            </p>
-                            <p class="text-muted">
-                                Sistema de Gestión interno, cual quier problema con el sistema favor de notificarlo al
-                                area de desarrollo Intranet Dime V0.00001
-                                <!-- <a href="pages-register.html" class="text-dark ms-1"><b>Sign Up</b></a> -->
-                            </p>
-                        </div>
-                        <!-- end col -->
-                    </div>
-                    <!-- end row -->
-                </div>
-                <!-- end col -->
+                </div> <!-- end col -->
             </div>
             <!-- end row -->
         </div>
         <!-- end container -->
     </div>
     <!-- end page -->
+
+    <!-- Vendor js -->
     <script src="./js/vendor.min.js"></script>
+
+    <!-- App js -->
     <script src="./js/app.min.js"></script>
+
 </body>
 
 </html>
